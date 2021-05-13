52b641c5c2d3d7af8d1e6d88a96199e4541caec5
Alvaro Lopez <lt0895032020@unab.edu.sv>
2021-05-13 16:15:09 -0600

    ejercicio

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dato1 = 16;
+            String dato2 = "25g";
+            int resultado = dato1 + Convert.ToInt32(dato2);
+
+            MessageBox.Show("Lo sentimos no se pudo realizar la operación solicitada");
+        }
+    }
+}
